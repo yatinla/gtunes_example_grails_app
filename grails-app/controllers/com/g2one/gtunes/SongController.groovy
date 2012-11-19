459bb027576a7b803da617c2bbac94e64b7c23b5
yatinla <mike@taylorwebhome.org>
2012-11-19 07:49:38 -0800

    Hopefull will work with scaffold now

---
+++ grails-app/controllers/com/g2one/gtunes/SongController.groovy
@@ -0,0 +1,7 @@
+package com.g2one.gtunes
+
+class SongController {
+
+    //def index() { }
+    def scaffold = Song
+}
--- grails-app/domain/com/g2one/gtunes/Song.groovy
+++ grails-app/domain/com/g2one/gtunes/Song.groovy
@@ -1,7 +1,9 @@
 package com.g2one.gtunes
 
 class Song {
-
+    String title
+    String artist
+    Integer duration
     static constraints = {
     }
 }
